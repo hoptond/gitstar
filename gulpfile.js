720655e378d263c17abe7b6afd9ed2ed999b0f91
Daniel <hoptond848@protonmail.com>
2018-10-25 17:10:26 +0100

    add very basic result output

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp')
+var sourcemaps = require('gulp-sourcemaps')
+var babel = require("gulp-babel");
+
+gulp.task("babel", function () {
+    return gulp.src("js/*.js")
+        .pipe(babel())
+        .pipe(gulp.dest("dist"));
+})
+
+
+gulp.task('babel:watch', function () {
+    gulp.watch('js/*.js', ['babel'])
+})
+
+// gulp.task('default', function() {
+//     gulp.start('babel:watch')
+// })
--- js/star.js
+++ js/star.js
@@ -1,6 +1,14 @@
-async function getData() {
-    let response = await fetch('')
+async function getPopularRepos() {
+    let response = await fetch('https://api.github.com/search/repositories?q=e&sort=stars&order=desc')
+    let data = await response.json()
+    let repos = data.items.slice(0, 3)
+    console.log(repos)
 
-
-    await return data;
+    var display = document.getElementById('repos')
+    repos.forEach(function(repo) {
+        display.innerText += repo.name;
+    })
 }
+getPopularRepos()
+
+
