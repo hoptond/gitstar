92c586ce603612cb67dc369cce7dbae88d835bc5
Daniel <hoptond848@protonmail.com>
2018-10-26 11:23:01 +0100

    add sass implementation and styling

--- js/star.js
+++ js/star.js
@@ -2,7 +2,7 @@ async function getPopularRepos(name, startdate, enddate) {
     let response = await fetch(`https://api.github.com/search/repositories?q=${name} pushed:${startdate}..${enddate}&sort=stars`)
     let data = await response.json()
     let repos = data.items.slice(0, 3)
-    var results = document.getElementById('repos')
+    var results = document.getElementById('results')
     repos.forEach(function(repo) {
         let listItem = document.createElement('li')
         listItem.innerText = repo.name
@@ -11,7 +11,7 @@ async function getPopularRepos(name, startdate, enddate) {
 }
 
 function clearResults() {
-    var results = document.getElementById('repos')
+    var results = document.getElementById('results')
     while(results.firstChild) {
         results.removeChild(results.firstChild)
     }
@@ -21,5 +21,6 @@ document.querySelector('#submit').addEventListener('click', function (e) {
     var formData = new FormData(e.target.form)
     clearResults()
     getPopularRepos(formData.get('name'), formData.get('startdate'), formData.get('enddate'))
+    document.querySelector('h4').classList.remove('hidden')
     e.preventDefault()
 })
