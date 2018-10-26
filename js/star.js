2f169e0706672daee2a5056b288922d45ea199f0
Daniel <hoptond848@protonmail.com>
2018-10-26 09:37:49 +0100

    update js/star.js

--- js/star.js
+++ js/star.js
@@ -1,5 +1,5 @@
 async function getPopularRepos() {
-    let response = await fetch('https://api.github.com/search/repositories?q=e&sort=stars&order=desc')
+    let response = await fetch('https://api.github.com/search/repositories?sort=stars&q=pushed:2017-01-01..2017-12-31')
     let data = await response.json()
     let repos = data.items.slice(0, 3)
     var display = document.getElementById('repos')
